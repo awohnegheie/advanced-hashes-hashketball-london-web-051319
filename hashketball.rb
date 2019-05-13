38a9232bac2aca35437e1b5f6eb466f46ef1f0f3
Awoh Negheie <awohnegheie@gmail.com>
2019-05-13 09:35:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -120,12 +120,12 @@ def game_hash
 end
 
 
-def shoe_size("Bismack Biyombo")
+def shoe_size()
   player = find_the_player(name)
   player.fetch(:shoe)
 end
 
-def num_points_scored("Bismack Biyombo")
+def num_points_scored()
   player = find_the_player(name)
   player.fetch(:points)
 end
